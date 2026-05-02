f940c390ff3ba8e11fae662dc0d0ff662cd7460a
Daniil <daniil.official@internet.ru>
2026-05-02 19:54:02 +0300

    FIx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,7 +7,7 @@ export function Dashboard() {
   return (
     <Routes>
       <Route index element={<DashboardLayout />} />
-      <Route path="trades" element={<TradeList />} />
+      <Route path="trades" element={<TradeList trades={[]} />} />
       <Route path="wallets" element={<WalletConnect />} />
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
